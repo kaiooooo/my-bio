c3e4bfc0dabc79c74066c61a8628738fa101cc7b
kaio <kaitozhiroki@gmail.com>
2025-02-10 18:26:28 -0300

    HTML. CSS. JS
    autoplay music, new effects

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ document.querySelector(".botao-pause").addEventListener("click", pausarMusica);
 
 musica.addEventListener("timeupdate", atualizarBarra);
 
-function tocarMusica() {
-  musica.play();
-  document.querySelector(".botao-pause").style.display = "block";
-  document.querySelector(".botao-play").style.display = "none";
-}
-
 function pausarMusica() {
   musica.pause();
   document.querySelector(".botao-pause").style.display = "none";
   document.querySelector(".botao-play").style.display = "block";
+}
+
+function tocarMusica() {
+  musica.play();
+  document.querySelector(".botao-play").style.display = "none";
+  document.querySelector(".botao-pause").style.display = "block";
 }
 
 function atualizarBarra() {
